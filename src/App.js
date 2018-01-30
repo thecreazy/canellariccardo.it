c61c041ce054bcd6c1dcff0e796e1e2eec1867d9
riccardo.canella <riccardo.canella@caffeina.com>
2018-01-30 15:06:57 +0100

    added styled-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Hello from '@/components/Hello'
 
+
 export default class App extends Component {
 
 	render() {
-		return <Hello>Hello React World! ✨</Hello>
+		return <Hello>Hello Canella Riccardo World! ✨</Hello>
 	}
 
 }
--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
 const Hello = styled.h1`
-
-`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 export default Hello
