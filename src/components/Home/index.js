5213a33a8d3c4f9aaa10965e061f96dcb45b85b9
Riccardo Canella <canella_riccardo@icloud.com>
2018-03-12 22:57:57 +0100

    sass

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => (
+  <section id="home">
+    <h1>Riccardo Canella</h1>
+    <p className="__midwidth">From Italy living in Parma, where I work as a Frontend Developer at Caffeina. With a fullstack javascript background, my strength lies in websites and apps. If you want to know more about me, my work or if youʼre a head hunter who wants to offer me a lot of money, feel free to contact me on canella_riccardo[at]icloud.com.</p>
+    <h2>ON THE ROAD DEVELOPER</h2>
+  </section>
+)
+
+export default Home
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,29 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
 import './index.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Riccardo Canella - On the road developer"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Riccardo Canella site' },
+        { name: 'keywords', content: 'riccardo, canella, riccardocanella, riccardo canella, on the road developer' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono" rel="stylesheet" />
+    {children()}
   </div>
 )
 
