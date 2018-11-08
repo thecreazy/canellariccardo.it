4975cf3755d60831bc2e041b22a569ff90904ffa
Riccardo Canella <canella_riccardo@icloud.com>
2018-11-09 00:22:29 +0100

    jobtome

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -114,9 +114,9 @@ export default class Home extends Component {
 				<div className="homecontainer __animation-opacity" id="homecontainer">
 					<h1>Riccardo Canella</h1>
 					<p className="homecontainer__description">
-						From Italy living in Parma, where I work as a Frontend Developer at{' '}
-						<a href="https://caffeina.com" target="_blank" rel="noopener noreferrer">
-							Caffeina
+						From Italy living in Varese, where I work as a Frontend Developer at{' '}
+						<a href="https://weare.jobtome.com" target="_blank" rel="noopener noreferrer">
+							Jobtome
 						</a>. With a fullstack javascript background, my strength lies in websites and apps. If you want
 						to know more about me, my work or if youʼre a head hunter who wants to offer me a lot of money,
 						feel free to contact me on canella_riccardo[at]icloud.com.
